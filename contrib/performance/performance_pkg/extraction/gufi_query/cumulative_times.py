19a906851a2311bb8f0b0f9f48e8ca17eabc963a
Btslade <btslade97@gmail.com>
2023-02-07 12:25:23 -0500

    Column Format 4 added

--- contrib/performance/performance_pkg/extraction/gufi_query/cumulative_times.py
+++ contrib/performance/performance_pkg/extraction/gufi_query/cumulative_times.py
@@ -167,7 +167,7 @@ COLUMN_FORMAT_2=  [
     ['Real time (main)',                           float],
 ]
 
-# ordered cumulative times columns (commit 4164985 "change querydb macro into a function" -> ???)
+# ordered cumulative times columns (commit 4164985 "change querydb macro into a function" -> commit 7cd35f8 "xattrprep from Gary")
 COLUMN_FORMAT_3=  [
 
     # from gufi_query
@@ -219,13 +219,63 @@ COLUMN_FORMAT_3=  [
     ['Total Thread Time (not including main)',     float],
 ]
 
+# ordered cumulative times columns (commit 216ef5b accidentally added argument to -w flag -> ???)
+COLUMN_FORMAT_4=  [
+
+    # from gufi_query
+    ['set up globals',                             float],
+    ['set up intermediate databases',              float],
+    ['thread pool',                                float],
+    ['open directories',                           float],
+    ['open databases',                             float],
+    ['sqlite3_open_v2',                            float],
+    ['set pragmas',                                float],
+    ['load extensions',                            float],
+    ['addqueryfuncs',                              float],
+    ['descend',                                    float],
+    ['check args',                                 float],
+    ['check level',                                float],
+    ['check level <= max_level branch',            float],
+    ['while true',                                 float],
+    ['readdir',                                    float],
+    ['readdir != null branch',                     float],
+    ['strncmp',                                    float],
+    ['strncmp != . or ..',                         float],
+    ['snprintf',                                   float],
+    ['lstat',                                      float],
+    ['isdir',                                      float],
+    ['isdir branch',                               float],
+    ['access',                                     float],
+    ['set',                                        float],
+    ['clone',                                      float],
+    ['pushdir',                                    float],
+    ['attach intermediate databases',              float],
+    ['check if treesummary table exists',          float],
+    ['sqltsum',                                    float],
+    ['sqlsum',                                     float],
+    ['sqlent',                                     float],
+    ['detach intermediate databases',              float],
+    ['close databases',                            float],
+    ['close directories',                          float],
+    ['restore timestamps',                         float],
+    ['free work',                                  float],
+    ['output timestamps',                          float],
+    ['aggregate into final databases',             float],
+    ['print aggregated results',                   float],
+    ['clean up globals',                           float],
+    ['Rows returned',                                int],
+    ['Queries performed',                            int],
+    ['Real time',                                  float],
+    ['Total Thread Time (not including main)',     float],
+]
+
 COLUMNS_COMBINED = [
     # not from gufi_query
     ['id',                                          None],
     ['commit',                                       str],
     ['branch',                                       str],
 
-] + COLUMN_FORMAT_1 + COLUMN_FORMAT_2 + COLUMN_FORMAT_3
+] + COLUMN_FORMAT_1 + COLUMN_FORMAT_2 + COLUMN_FORMAT_3 + COLUMN_FORMAT_4
 
 # Remove duplicate entries
 COLUMNS = []
@@ -235,7 +285,7 @@ def create_table(con):
     common.create_table(con, TABLE_NAME, COLUMNS)
 
 def extract(src, commit, branch):
-    return common.cumulative_times_extract(src, commit, branch, COLUMNS, [COLUMN_FORMAT_1, COLUMN_FORMAT_2, COLUMN_FORMAT_3])
+    return common.cumulative_times_extract(src, commit, branch, COLUMNS, [COLUMN_FORMAT_1, COLUMN_FORMAT_2, COLUMN_FORMAT_3, COLUMN_FORMAT_4])
 
 def insert(con, parsed):
     common.insert(con, parsed, TABLE_NAME, COLUMNS)
